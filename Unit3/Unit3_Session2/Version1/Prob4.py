78a448f683cfa85710dddd61b80a05ba04c36278
kyileiaye2021 <98507949+kyileiaye2021@users.noreply.github.com>
2024-06-22 19:18:22 +0000

    More files to unit 3

--- Unit3/Unit3_Session2/Version1/Prob4.py
+++ Unit3/Unit3_Session2/Version1/Prob4.py
@@ -3,3 +3,21 @@
 #ver 1
 #prob 4
 
+def longest_uniform_substring(s):
+  count = 1
+  longest = 1
+  for i in range(1, len(s)):
+    if s[i] == s[i-1]:
+      count +=1 
+      longest = max(longest, count)
+    else:
+      count = 1
+  return longest
+
+s1 = "aabbbbCdAA"
+l1 = longest_uniform_substring(s1)
+print(l1)
+
+s2 = "abcdef"
+l2 = longest_uniform_substring(s2)
+print(l2)
--- Unit3/Unit3_Session2/Version1/Prob5.py
+++ Unit3/Unit3_Session2/Version1/Prob5.py
@@ -2,3 +2,23 @@
 #session 2
 #ver 1
 #prob 5
+
+#everytime, Ashe is attacked, the duration will be reset to its original val
+#Once attacked, he will be poisoned for the duration
+#check when it is attacked
+  #decremented duration by 1
+
+def find_poisoned_duration(time_series, duration):
+  total_poisoned_time = 0
+  for i, time in enumerate(time_series):
+    if i > 0:
+      if time - time_series[i-1] <= duration:
+        total_poisoned_time += (time - time_series[i-1]) - 1
+      else:
+        total_poisoned_time += 3
+
+  return (total_poisoned_time + 3)
+
+time_series = [1,4,9]
+damage = find_poisoned_duration(time_series, 3)
+print(damage)
