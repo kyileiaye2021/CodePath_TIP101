1d4fe3a2b4ed29e2e7d2083b221cae80c925ca09
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-06-27 03:19:07 +0000

    evaluatiing time and space complexity of problem 3

--- Unit4/Session1/Version1/Prob3.py
+++ Unit4/Session1/Version1/Prob3.py
@@ -6,11 +6,13 @@
 #Time complexity - O(n)
 #Space complexity - O(n)
 
-# lst = [1, 2, 3, 4, 5]
-# print(reverse_list(lst))
 def reverse_list(lst):
   # Create a new reversed list
   reversed_lst = lst[::-1]
   # Copy the elements back into the original list
   for i in range(len(lst)):
       lst[i] = reversed_lst[i]
+  return lst
+
+lst = [1, 2, 3, 4, 5]
+print(reverse_list(lst))
