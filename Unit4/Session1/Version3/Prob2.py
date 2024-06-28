6916b1614d47669617c12f3c9d8285b4037c59c1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-06-28 18:28:46 +0000

    solving problem of two sum II

---
+++ Unit4/Session1/Version3/Prob2.py
@@ -0,0 +1,51 @@
+#unit 4
+#session 1
+#ver3
+#prob 2
+
+# Assumption
+# list is sorted
+# Every sum of two ele must be unique
+
+# Hight level planning
+# * Iterative Approach O(n^2)
+# * Two pointer technique I
+#   * create dict to store the ele and index 
+#   * iterate over the list and subtract the curr ele from the target. find the diff in the dict and return index
+# * Two pointer technique II
+#   * itearte over the lst until l passes r
+#   * shift the l and r around according to the addition
+
+# Low level planning
+# * create l and r 
+# * create a list
+# iterate over the list until l passes r
+#    * find the addition of l and r
+#    * check if the addition is greater than target
+#        * move r to left by 1
+#    * if the addition is less than the target
+#        * move l to right by 1
+#    *otherwise, add l and r to the list
+# return the list
+
+
+def two_sum(nums, target):
+  l, r = 0, len(nums) -1
+  target_sum_index_lst = []
+
+  while l < r:
+    addition = nums[l] + nums[r]
+    if addition > target:
+      r -= 1
+    elif addition < target:
+      l += 1
+    else:
+      target_sum_index_lst.append(l)
+      target_sum_index_lst.append(r)
+      return target_sum_index_lst
+  
+nums = [2,7,11,15]
+sol1 = two_sum(nums, 9)
+print(sol1)
+sol2 = two_sum(nums, 18)
+print(sol2)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-def find_highest_exponent(base, limit):
-  i = 1
-  while base ** i < limit:
-    i += 1
-  return i-1
+def two_sum(nums, target):
+  l, r = 0, len(nums) -1
+  target_sum_index_lst = []
 
-exp = find_highest_exponent(2, 100)
-print(exp)
+  while l < r:
+    addition = nums[l] + nums[r]
+    if addition > target:
+      r -= 1
+    elif addition < target:
+      l += 1
+    else:
+      target_sum_index_lst.append(l)
+      target_sum_index_lst.append(r)
+      return target_sum_index_lst
 
-exp2 = find_highest_exponent(3, 5)
-print(exp2)
+nums = [2,7,11,15]
+sol1 = two_sum(nums, 9)
+print(sol1)
+sol2 = two_sum(nums, 18)
+print(sol2)
