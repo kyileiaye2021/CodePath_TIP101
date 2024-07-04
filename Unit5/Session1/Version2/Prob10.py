daa1422f70a46244b054a084ce299f3dd08e1da5
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-04 19:36:05 +0000

    Solved Middle Node problem

--- Unit5/Session1/Version2/Prob10.py
+++ Unit5/Session1/Version2/Prob10.py
@@ -1,8 +1,13 @@
+#unit 5
+#session 1
+#ver 2
+#Prob 10
+
 class Node:
   def __init__(self, value, next=None):
     self.value = value
     self.next = next
-
+    
 tail = Node(200)
 middle = Node(150, tail)
 head = Node(100,middle)
