3ac62273ed0f2c6248c5ce418fecb3fb6e0c3825
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-04 00:45:24 +0000

    Prob 5

--- Unit5/Session1/Version2/Prob5.py
+++ Unit5/Session1/Version2/Prob5.py
@@ -2,3 +2,30 @@
 #session 1
 #ver 2
 #prob 5
+
+
+class Card():
+  def  __init__(self, suit, rank):
+    self.suit = suit
+    self.rank = rank
+
+  def get_value(self):
+    int_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+    if self.rank in int_ranks:
+      return int(self.rank)
+    elif self.rank == 'Ace':
+      return 1
+    elif self.rank == "Jack":
+      return 11
+    elif self.rank == "Queen":
+      return 12
+    elif self.rank == "King":
+      return 13
+    else:
+      return False
+
+card = Card("Hearts", "7")
+print(card.get_value())
+
+card_two = Card("Spades", "Jack")
+print(card_two.get_value())
