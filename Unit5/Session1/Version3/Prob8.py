89c8ec835e2995ed5dc425850bfbc670e066f1f8
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-05 22:20:55 +0000

    Solved Get Ranked problem

--- Unit5/Session1/Version3/Prob8.py
+++ Unit5/Session1/Version3/Prob8.py
@@ -1,3 +1,8 @@
+#unit 5
+#session 1
+#ver 3
+#Prob 8
+
 class Player:
   def __init__(self, character, kart, opponent=None):
       self.character = character
@@ -7,22 +12,21 @@ class Player:
 
 def get_place(my_player):
   current_player = my_player
-  number = 0
+  number = 1
   while current_player:
     number += 1
     current_player = current_player.ahead
-  return number
 
 peach = Player("Peach", "Daytripper")
 mario = Player("Mario", "Standard Kart M", peach)
 luigi = Player("Luigi", "Super Blooper", mario)
 
-player1_rank = get_place(luigi)
+player1_rank = get_place("Luigi")
 print(player1_rank)
 
-player2_rank = get_place(peach)
+player2_rank = get_place("Peach")
 print(player2_rank)
 
-player3_rank = get_place(mario)
+player3_rank = get_place("Mario")
 print(player3_rank)
 
