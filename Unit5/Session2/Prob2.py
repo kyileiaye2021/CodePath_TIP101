fda31e74a5055e1de0a3e9dc9496b4ed5f9418c2
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-06 21:45:09 +0000

    Solved Convert to linked list problem

---
+++ Unit5/Session2/Prob2.py
@@ -0,0 +1,15 @@
+#unit 5
+#session 2
+#ver 1
+#Prob 2
+
+class Node:
+  def __init__(self, value, next=None):
+    self.value = value
+    self.next = next
+
+node_2 = Node("Wigglytuff")
+node_1 = Node("Jigglypuff", node_2)
+
+print(node_1.value, "->", node_1.next.value)
+print(node_2.value, "->", node_2.next)
