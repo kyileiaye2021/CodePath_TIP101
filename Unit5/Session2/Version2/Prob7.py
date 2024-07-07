3b1bd54d711353c55254622329ecc0bab1fceede
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-07 18:33:16 +0000

    Solved Pop Node problem

--- Unit5/Session2/Version2/Prob7.py
+++ Unit5/Session2/Version2/Prob7.py
@@ -1,3 +1,8 @@
+#unit 5
+#session 2
+#ver 2
+#prob 7
+
 class Node:
   def __init__(self, value, next=None):
     self.value = value
@@ -20,8 +25,14 @@ def ll_pop(head, i):
       print(head.value)
     head = head.next
 
-
 num3 = Node(3)
 num2 = Node(2, num3)
 num1 = Node(1, num2)
 ll_pop(num1, 1)
+'''
+Example Usage:
+
+# linked list: num1 -> num2 -> num3
+ll_pop(num1, 1)
+# result: num1 -> num3
+'''
