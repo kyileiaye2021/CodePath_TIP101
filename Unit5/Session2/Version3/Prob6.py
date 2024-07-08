85fbb264abb88c28c6eb124868a8b92de7ba7a78
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-08 23:43:14 +0000

    Solved Find Minimum in Linked List problem

--- Unit5/Session2/Version3/Prob6.py
+++ Unit5/Session2/Version3/Prob6.py
@@ -1,3 +1,8 @@
+#unit 5
+#session 2
+#ver 3
+#prob 6
+
 class Node:
   def __init__(self, value, next=None):
       self.value = value
@@ -10,7 +15,7 @@ def find_min(head):
     if curr_head.value < curr_min_val:
       curr_min_val = curr_head.value
     curr_head = curr_head.next
-
+    
   return curr_min_val
 
 
@@ -18,5 +23,4 @@ node_4 = Node(7)
 node_3 = Node(6, node_4)
 node_2 = Node(5, node_3)
 node_1 = Node(8,node_2)
-
 print(find_min(node_1))
