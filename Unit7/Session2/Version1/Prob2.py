aa2572b4cb5b6c212ad8aaa9a594bce2fd564af2
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-07-20 19:07:38 +0000

    Updated How many 1s problem

--- Unit7/Session2/Version1/Prob2.py
+++ Unit7/Session2/Version1/Prob2.py
@@ -25,7 +25,8 @@ def count_ones(lst):
     else:
       right = mid - 1
 
-  return len(lst) - left
+  if left < len(lst):
+    return len(lst) - left
 
 print(count_ones([0, 0, 0, 0, 1, 1, 1]))#3
 print(count_ones([0,0,1,1,1,1])) #4
