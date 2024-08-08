b35644c5db58ed281d68b39e12a7effcbc9360e1
Kyi Lei Aye (She/ Her) <98507949+kyileiaye2021@users.noreply.github.com>
2024-08-08 03:07:59 +0000

    Updated Best Time to Buy & Sell Stock

---
+++ Unit10/Session1/Version1/Prob2.py
@@ -0,0 +1,35 @@
+#Problem 2: Best Time to Buy & Sell Stock
+
+# create two pointers (l, r = 0, 1)
+#Iterate through the list
+#   if r val is greater than l val
+#     calculate the profit and get the max val
+#   else:
+#     move l to r
+#   increment r by 1
+
+def max_profit(prices):
+  maximum_profit = 0
+  l, r = 0, 1
+
+  while r < len(prices):
+    if prices[r] < prices[l]:
+      l = r
+
+    else:
+      profit = prices[r] - prices[l]
+      maximum_profit = max(maximum_profit, profit)
+
+    r += 1
+
+  return maximum_profit
+
+
+prices = [7,1,5,3,6,4]
+print(max_profit(prices))
+
+prices = [7,6,4,3,1]
+print(max_profit(prices))
+
+# Time complexity - O(n)
+# Space complexity - O(1) 
